2478ccc8437c1c9c19a0f794d8ceb7980e10f805
differentsmoke <pablo.barria@post.harvard.edu>
2018-04-22 19:31:18 -0400

    Added ES harmony option to Uglifier to use ES6 in production

--- app/controllers/floods_controller.rb
+++ app/controllers/floods_controller.rb
@@ -1,41 +1,42 @@
+# frozen_string_literal: true
+
 class FloodsController < ApplicationController
-    before_action :authenticate_admin!, only: [:index]
-    def index
-        @floods = Flood.all
-    end
+  before_action :authenticate_admin!, only: [:index]
+  def index
+    @floods = Flood.all
+  end
 
-    def new
-        @flood = Flood.new
-        @flood.attachments.build
-    end
+  def new
+    @flood = Flood.new
+    @flood.attachments.build
+  end
 
-    def create
-        @flood = Flood.new flood_params
-        if @flood.save
-            flash[:notice] = "Thank you for contributing data!"
-            redirect_to action: :index
-          else
-            flash.now[:error] = "Oops. There were errors:\n#{@flood.errors.full_messages.to_sentence}"
-            render :new
-          end
-    end
+  def create
+    @flood = Flood.new flood_params
+    if @flood.save
+      flash[:notice] = 'Thank you for contributing data!'
+      redirect_to action: :index
+    else
+      flash.now[:error] = "Oops. There were errors:\n#{@flood.errors.full_messages.to_sentence}"
+      render :new
+      end
+  end
 
-    private
+  private
 
-    def flood_params
-        params.require(:flood).permit(
-            :first_name,
-            :last_name,
-            :email,
-            :date_of_report,
-            :description,
-            :latitude,
-            :longitude,
-            attachments_attributes: [
-                :media,
-                :attachment
-              ]
-        )
-    end
-
+  def flood_params
+    params.require(:flood).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :date_of_report,
+      :description,
+      :latitude,
+      :longitude,
+      attachments_attributes: %i[
+        media
+        attachment
+      ]
+    )
+  end
 end
